5acb56009a39016b03079967c33b6260ee8f154f
Mares <66750744+mareszhar@users.noreply.github.com>
2026-04-06 21:24:15 -0400

    🔧 disable devtools
    supresses vite's warning about unoptimized deps.
    
    it's possible to silence those warnings with this config for `vite` in `nuxt.config.ts`:
    
    ```ts
    optimizeDeps: {
      include: [
        '@vue/devtools-core',
        '@vue/devtools-kit',
      ],
    }
    ```
    
    however, i want to keep the demo simple. so, to reduce the noise those warnings add to the terminal, i'll just disable nuxt devtools for this demo.

--- demo/nuxt.config.ts
+++ demo/nuxt.config.ts
@@ -3,8 +3,8 @@ import process from 'node:process'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  compatibilityDate: '2026-04-06',
+  devtools: { enabled: false },
   vite: {
     css: {
       preprocessorOptions: {
